2f61a1d282582698a42b9ca8771464854d24e4c6
David Christian <david.w.christian@gmail.com>
2014-10-17 12:18:18 -0700

    set up votes as join table

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
   has_many   :votes
+  has_many   :users, through: :votes
   validates  :title, presence: true
   validates  :description, presence: true
   validates  :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 has_secure_password
 validates_presence_of :password, :on => :create
 has_many :votes
-has_many :activities
+has_many :activities, through: :votes
 end
