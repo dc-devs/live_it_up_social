8d4c05d2bed1bc2378dba29b63b7419774f412fc
andrewmcdonald2 <andrwmcdnld2@gmail.com>
2014-10-16 21:46:22 -0700

    added sessions controller, session index page view and user login authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017001305) do
+ActiveRecord::Schema.define(version: 20141017025225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(version: 20141017001305) do
     t.integer  "event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "motto"
+    t.string   "picture_url"
+    t.string   "professional_blurb"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "votes", force: true do |t|
