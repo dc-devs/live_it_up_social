11ce03c935dcfb59818eae4313974fbf430f327e
andrewmcdonald2 <andrwmcdnld2@gmail.com>
2014-10-17 18:13:51 -0700

    fixed js file that flips the bakcgorund. background is no longer retrieved from foreign urls but now lives in images/ folder. Also included activities controller logic that sends you to a different page depending on the day of the week. added extra activities views (blank files) that serve as placeholders for friday and saturday views

--- app/assets/javascripts/background_flipper.js
+++ app/assets/javascripts/background_flipper.js
@@ -1,18 +1,10 @@
 $( document ).ready(function(){
 
-var background_images = [
-"http://www.fullhdwpp.com/wp-content/uploads/San-Francisco-Tramline_www.FullHDWpp.com_.jpg",
+var background_images = ["cable_car.jpg", "pier.jpg", "main_background.jpg", "fort_mason.jpg","ocean_beach.jpg"]
 
-"http://hdwallpaper.freehdw.com/0007/animals_hdwallpaper_san-francisco-pier_61190.jpg",
-
-"http://wallhud.com/download?filename=2013/04/San-Francisco-Bridge-Wallpaper-HD.jpg",
-
-"http://4.bp.blogspot.com/-2jX-A7hI0L8/UQWah2wrf0I/AAAAAAAABoo/EC6sjOW_ziU/s1600/pat1456664812.jpg",
-
-"http://upload.wikimedia.org/wikipedia/commons/1/1f/Ocean_Beach_in_San_Francisco_at_sunrise.jpg"]
-
-  if (document.getElementById("homepage_body")){
-    $("#homepage_body").css("background-image", "url("+background_images[[Math.floor(Math.random()*background_images.length)]]+")")
-  }
+if (document.getElementById("homepage_body")){
+  $("#homepage_body").css("background-image", "url(assets/"+background_images[[Math.floor(Math.random()*background_images.length)]]+")")
+}
 
 })
+
