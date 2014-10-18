50c431a5e5a285762ab8cad19e93440903f74518
andrewmcdonald2 <andrwmcdnld2@gmail.com>
2014-10-17 21:19:39 -0700

    added logic to move counter forwrd to the next friday. update controller logic to redirect based upon the day sof the week.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
-
   def current_user
     @_current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
