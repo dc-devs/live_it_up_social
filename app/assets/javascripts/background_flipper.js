a800eddfe8434d8a65bdb667a4b817d22c035037
Melissa Porto <melroman26@gmail.com>
2014-10-19 11:35:47 -0700

    Added scrolling affect for homepage with background image fixed.

--- app/assets/javascripts/background_flipper.js
+++ app/assets/javascripts/background_flipper.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
 var background_images = ["cable_car.jpg", "pier.jpg", "main_background.jpg", "fort_mason.jpg","ocean_beach.jpg"]
 
 if (document.getElementById("homepage_body")){
-  $("#homepage_body").css("background", "url(assets/"+background_images[[Math.floor(Math.random()*background_images.length)]]+") no-repeat")
+  $("#homepage_body").css("background", "url(assets/"+background_images[[Math.floor(Math.random()*background_images.length)]]+") no-repeat fixed")
 }
 
 })
