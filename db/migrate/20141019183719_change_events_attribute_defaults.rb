0aa56585aa9383c9db00eaa8eeb4e0f9b7d44d40
David Christian <david.w.christian@gmail.com>
2014-10-19 17:58:50 -0700

    added login signup branch

--- db/migrate/20141019183719_change_events_attribute_defaults.rb
+++ db/migrate/20141019183719_change_events_attribute_defaults.rb
@@ -3,6 +3,5 @@ class ChangeEventsAttributeDefaults < ActiveRecord::Migration
     change_column :events, :start_time,  :string, default: "10:00 am"
     change_column :events, :date,  :string, default: "Saturday"
     change_column :events, :duration,  :string, default: "6 hours + drinking overtime"
-
   end
 end
