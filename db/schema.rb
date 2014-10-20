31b40fcb0877fc21358d3f6a2d8de2f0c4b219d5
David Christian <david.w.christian@gmail.com>
2014-10-20 14:21:50 -0700

    facebook oauth working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020062804) do
+ActiveRecord::Schema.define(version: 20141020195654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20141020062804) do
     t.string   "icon"
     t.string   "photo"
     t.text     "necessities"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authentications", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
