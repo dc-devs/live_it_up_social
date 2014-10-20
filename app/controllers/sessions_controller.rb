8e89eef3bff13b1ea5e435b377dd0913d8f5401d
Melissa Porto <melroman26@gmail.com>
2014-10-19 17:49:31 -0700

    Edited Signup form page Login modal Login sessions now works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to users_url, :notice => "Logged in!"
+    p params[:session][:email]
+    @user_email = params[:session][:email]
+    @user = User.find_by(:email => @user_email)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to @user, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
-      render 'index'
+      flash[:notice] = "Invalid email or password"
+      redirect_to new_session_path
     end
   end
 
