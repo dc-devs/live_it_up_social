1e91bdf587532ec69a8415f52acfd7707f80b7e4
Melissa Porto <melroman26@gmail.com>
2014-10-21 11:38:05 -0700

    -Production environment for welcome email -Scheduled monday morning emails (Still need to add in link for pics) -Basic styling of both emails

--- app/mailers/presentation_mailer.rb
+++ app/mailers/presentation_mailer.rb
@@ -1,11 +1,9 @@
 class PresentationMailer < ActionMailer::Base
   default to: Proc.new { User.pluck(:email) },
-          from: 'melroman26@gmail.com'
+          from: 'liveitupsocial@gmail.com'
 
-  def event_recap(user)
-    @user = user
+  def event_recap
     mail(subject: "Weekend Recap")
-
   end
 end
 
