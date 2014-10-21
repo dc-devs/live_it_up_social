cda960f8baa7ed492bf66e1ce8792ab886b4d395
alexander <alexandr.larionov88@gmail.com>
2014-10-21 12:31:47 -0700

    fixed recent tab. Now it shows all activities ordered by created day is desc order test added for Actvity model to test sorting test added for Photo model

--- spec/controllers/votes_spec.rb
+++ spec/controllers/votes_spec.rb
@@ -12,13 +12,6 @@ describe VotesController do
       expect(response).to render_template("index")
     end
 
-    it "assigns a new Vote to @vote" do
-      # Activity.create(id: 2, title: "biking", description: "biking", location: "SF", category: "biking")
-      # current_user = User.create(id: 1, first_name: "andrew", last_name: "mcdonald", username: "andrew2", motto: "yay", photo: "www.ohot.com", professional_blurb: "yolo", password: "12345", phone_number: "12344444444", remaining_votes: 5)
-      # post :create, vote: {'user_id' => 1, 'activity_id' => 2}
-      # p Vote.count
-      # # expect(Vote.count).to eq(1)
-    end
   end
 end
 
