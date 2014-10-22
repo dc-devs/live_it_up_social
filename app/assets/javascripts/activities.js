9ed0629b69a110b1a321ed0c3cf693b0d1fc4af5
David Christian <david.w.christian@gmail.com>
2014-10-21 20:53:26 -0700

    basic arrow built for vote up

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -4,4 +4,16 @@ $(document).ready(function ($) {
   $('#scroll-eventDeclines').perfectScrollbar();
   $('#scroll-eventInvited').perfectScrollbar();
   $('#scroll-eventAccepted').perfectScrollbar();
+  
+  // $('.activities-ArrowUp').hover( handlerIn, handlerOut )
+
+  //   console.log($('.activities-voteUpButton'));
+  // function handlerIn(){
+  //   $('.activities-ArrowUp').fadeOut();
+  // }
+
+  // function handlerOut(){
+  //   $('.activities-ArrowUp').fadeIn();
+  // }
+
 });
