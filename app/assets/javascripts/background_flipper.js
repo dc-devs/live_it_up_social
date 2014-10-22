a7ffeaf6e0006393e76730bf3346b6122c67def3
Melissa Porto <melroman26@gmail.com>
2014-10-22 11:30:53 -0700

    *Homepage to have same polaroid layout as events page *Changed font on polaroids

--- app/assets/javascripts/background_flipper.js
+++ app/assets/javascripts/background_flipper.js
@@ -3,9 +3,18 @@ $( document ).ready(function(){
   var background_images = ["cable_car.jpg", "pier.jpg", "main_background.jpg", "fort_mason.jpg","ocean_beach.jpg"]
 
   if (document.getElementById("homepage_body")){
-  
+
     $("#homepage_body").css("background", "url(assets/"+background_images[[Math.floor(Math.random()*background_images.length)]]+") no-repeat").addClass("parallax");
-    
+
+//Trying to fix navbar
+
+(function(){
+    $(".navbar").mouseover(function() {
+    $(".navbar-header").show();
+  });
+  });
+//End of attempt
+
     $(document).scroll(function () {
       var y = $(this).scrollTop();
         if (y > 300) {
@@ -14,6 +23,7 @@ $( document ).ready(function(){
             $('.navbar').fadeOut();
         }
     });
+
 
   };
 
