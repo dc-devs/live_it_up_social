e4099de4b4c7ede436c43e146550912adb166fb6
alexander <alexandr.larionov88@gmail.com>
2014-10-22 15:55:29 -0700

    put search field inside the nav bar added error messages for user sign up, activity create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -30,15 +30,16 @@ class ActivitiesController < ApplicationController
     @vote       = Vote.new
     @activities = Activity.all
     @activity   = Activity.new(activity_params)
-    @activity.save
-
-    @sorted_by_vote = Activity.sort_acrtivities(@activities)
-    today = (Time.now.midnight..Time.now)
-    @trending_activities = Activity.where(created_at: today)
-    @recent_activities = Activity.where(created_at: today)
-    @trending_activities = Activity.sort_acrtivities(@trending_activities)
-
-    render :index
+    if @activity.save
+      @sorted_by_vote = Activity.sort_acrtivities(@activities)
+      today = (Time.now.midnight..Time.now)
+      @trending_activities = Activity.where(created_at: today)
+      @recent_activities = Activity.where(created_at: today)
+      @trending_activities = Activity.sort_acrtivities(@trending_activities)
+      render :index
+    else
+      render :new
+    end
   end
 
   def edit
