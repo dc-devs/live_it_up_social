1c76359c035813c7d5f374ee3a7d9c45ecc50a7b
alexander <alexandr.larionov88@gmail.com>
2014-10-21 21:05:36 -0700

    google map with marker

---
+++ app/assets/javascripts/google_map.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+      // var geocoder;
+      // var map;
+
+      // function initialize() {
+      //   geocoder = new google.maps.Geocoder();
+      //   var mapCanvas = document.getElementById('map_canvas');
+      //   myLatlng = new google.maps.LatLng(37.7833, -122.4167);
+      //   var mapOptions = {
+      //     center: myLatlng,
+      //     zoom: 12
+      //   }
+      //   map = new google.maps.Map(mapCanvas, mapOptions)
+
+      // address = $('#event-location').text();
+      // function codeAddress() {
+      //   geocoder.geocode( { 'address': address}, function(results, status) {
+      //     if (status == google.maps.GeocoderStatus.OK) {
+      //       map.setCenter(results[0].geometry.location);
+      //       var marker = new google.maps.Marker({
+      //           map: map,
+      //           position: results[0].geometry.location
+      //       });
+      //     } else {
+      //       alert('Geocode was not successful for the following reason: ' + status);
+      //     }
+      //   });
+      // }
+      // }
+      // google.maps.event.addDomListener(window, 'load', initialize);
+
+});
