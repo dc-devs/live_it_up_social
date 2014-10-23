d0c58ae4487055a7497941afc92ac65cd1208922
David Christian <david.w.christian@gmail.com>
2014-10-23 11:06:18 -0700

    changes to hp for final pressy

--- app/assets/javascripts/background_flipper.js
+++ app/assets/javascripts/background_flipper.js
@@ -1,34 +1,12 @@
 $( document ).ready(function(){
 
-  var background_images = ["https://s3-us-west-1.amazonaws.com/liveitupsocial/uploads/production/home/cable_car.jpg", "https://s3-us-west-1.amazonaws.com/liveitupsocial/uploads/production/home/main_background.jpg"]
+  var background_images = ["https://s3-us-west-1.amazonaws.com/liveitupsocial/uploads/production/home/main-bk.png"]
 
   if (document.getElementById("homepage_body")){
 
     $("#homepage_body").css("background", "url("+background_images[[Math.floor(Math.random()*background_images.length)]]+") no-repeat").addClass("parallax");
 
-//Trying to fix navbar to show on mouseover
 
-    // $(window).scroll(function() {
-    //     if ($(window).scrollTop() > 300) {
-    //         $('.navbar').fadeIn();
-    //     }
-    //     else {
-    //         $('.navbar').fadeOut();
-    //     }
-    // });
-
-    //   (function(){
-    //     $('.navbar').mouseover( function () {
-    //       $(this).fadeIn();
-    //       });
-
-    //     $('.navbar').mouseleave( function () {
-    //         if($(window).scrollTop() > 300) {
-    //             $(this).fadeOut();
-    //         }
-    //     });
-
-    // })
 //End of attempt (FAIL)
 
     $(document).scroll(function () {
@@ -52,7 +30,7 @@ $( document ).ready(function(){
     [].slice.call(parallax).forEach(function(el,i){
 
       var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+          elBackgrounPos = "0% " + (windowYOffset * speed) + "px";
 
       el.style.backgroundPosition = elBackgrounPos;
 
